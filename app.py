7d1bcea805f47cdebc7e4c803ff9360b6b5b94bb
zoepaidas <zkpaidas@terpmail.umd.edu>
2024-03-26 20:18:49 -0400

    creating of base form to create parent. creation of two child forms- favorite course which portrays someones favorite course when entered in the url and the other is contact which is a form that people can enter their information inyo snf iy it will portray it back to them

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,9 +14,29 @@ def hello():  # put application's code here
 @app.route('/about')
 def about():  # put application's code here
     return render_template('about.html')
+
 @app.route('/about-css')
 def about1():  # put application's code here
     return render_template('about-css.html')
+
+@app.route('/favorite-course', methods=['GET', 'POST'])
+def favorite_course():
+    print(f'Subject: {request.args.get("class_name")}')
+    print(f'Course Number: {request.args.get("course_number")}')
+    return render_template('favorite-course.html')
+
+@app.route('/base')
+def base():  # put application's code here
+    return render_template('base.html')
+
+@app.route('/contact', methods=["GET","POST"])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
+
 
 if __name__ == '__main__':
     app.run()
